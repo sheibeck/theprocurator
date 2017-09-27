877224890f6d9d8104e06390a56d330ad7d8aff3
Sterling Heibeck <sterling.heibeck@nocti.org>
2017-09-27 13:53:15 -0400

    3rd party auth integration

--- theprocurator/App_Start/Startup.Auth.cs
+++ theprocurator/App_Start/Startup.Auth.cs
@@ -7,6 +7,11 @@ using Microsoft.Owin.Security.Google;
 using Owin;
 using theprocurator.Data.Model;
 using theprocurator.Data;
+using Microsoft.Owin.Security.Twitter;
+using System.Configuration;
+using Microsoft.Owin.Security.Facebook;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace theprocurator
 {
@@ -51,19 +56,38 @@ namespace theprocurator
             //    clientId: "",
             //    clientSecret: "");
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            {
+                ConsumerKey = ConfigurationManager.AppSettings["twitter-consumer"],
+                ConsumerSecret = ConfigurationManager.AppSettings["twitter-secret"],
+                BackchannelCertificateValidator = new Microsoft.Owin.Security.CertificateSubjectKeyIdentifierValidator(new[]
+                {
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+                  "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                  "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                  "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary CA - G5
+                  "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                  "‎add53f6680fe66e383cbac3e60922e3b4c412bed", // Symantec Class 3 EV SSL CA - G3
+                  "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5", // VeriSign Class 3 Primary CA - G5
+                  "5168FF90AF0207753CCCD9656462A212B859723B", // DigiCert SHA2 High Assurance Server C‎A 
+                  "B13EC36903F8BF4701D498261A0802EF63642BC3"  // DigiCert High Assurance EV Root CA
+                })
+            });
+          
+            FacebookAuthenticationOptions facebookOptions = new FacebookAuthenticationOptions()
+            {
+                AppId = ConfigurationManager.AppSettings["facebook-consumer"],
+                AppSecret = ConfigurationManager.AppSettings["facebook-secret"],
+            };
+            facebookOptions.Scope.Add("email");
+            app.UseFacebookAuthentication(facebookOptions);
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = ConfigurationManager.AppSettings["googleplus-consumer"],
+                ClientSecret = ConfigurationManager.AppSettings["googleplus-secret"]
+            });
         }
     }
 }
---
+++ theprocurator/Helpers/EmailHelper.cs
@@ -0,0 +1,76 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace theprocurator.Helpers
+{
+    public class EmailHelper
+    {
+        /// <summary>
+        /// Sends the mail.
+        /// </summary>
+        /// <param name="subject">The subject.</param>
+        /// <param name="bodyHtml">The body HTML.</param>
+        /// <param name="bodyPlain">The body plain.</param>
+        /// <param name="mailTo">The mail to.</param>
+        /// <param name="mailFrom">The mail from.</param>
+        public static async Task<Response> SendMail(string mailTo, string templateId, string subject, Dictionary<string, string> substitutions)
+        {
+            var mailFrom = ConfigurationManager.AppSettings["TPN_Email_From"];
+
+            // get SendGrid info
+            var apiKey = ConfigurationManager.AppSettings["SendGridApiKey"];
+            var client = new SendGridClient(apiKey);
+
+            // set mail variables            
+            var from = new EmailAddress(mailFrom);
+            var to = new EmailAddress(mailTo);
+
+            // create an email
+            string bodyHtml = " ";
+            string bodyPlain = " ";
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, bodyPlain, bodyHtml);
+            msg.SetTemplateId(templateId);
+            msg.AddSubstitutions(substitutions);
+
+            // setup mail settings
+            msg.MailSettings = new MailSettings();
+
+            // turn on sandbox mode by default;
+            var enableSandbox = false;
+#if DEBUG
+            enableSandbox = true;
+#endif
+            msg.MailSettings.SandboxMode = new SandboxMode() { Enable = enableSandbox };
+        
+
+            // sendemail
+            return await client.SendEmailAsync(msg);
+        }
+
+        /// <summary>
+        /// Determines whether [is mail sent] [the specified mail].
+        /// </summary>
+        /// <param name="mail">The mail.</param>
+        /// <returns>
+        ///   <c>true</c> if [is mail sent] [the specified mail]; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsMailSent(Response mail)
+        {
+            switch (mail.StatusCode.ToString().ToLower())
+            {
+                case "ok":
+                case "accepted":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
